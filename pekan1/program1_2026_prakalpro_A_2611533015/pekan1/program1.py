72e88cbff1d0959cca5af40fc5f801457ea2156a
ZikriAdIRahman <zirkirahmane@gmail.com>
2026-09-07 22:20:00 +0700

    Add files via upload

---
+++ pekan1/program1_2026_prakalpro_A_2611533015/pekan1/program1.py
@@ -0,0 +1,11 @@
+"""
+halo ini cuman komentar biasa
+"""
+# ini komentar juga 
+
+print("Hallo Dunia")
+print()
+print("Program Ini Menghasilkan")
+print("Empat Baris Output")
+print("/||//\\\///\\\\")
+print("Program ini " + "Dibuat Oleh Zikri" + "2611533015")
