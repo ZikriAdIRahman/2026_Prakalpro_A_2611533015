d25f29c01daf259c5ab99d22ddde34f3bbd03d91
ZikriAdIRahman <zirkirahmane@gmail.com>
2026-09-09 20:52:16 +0700

    Add files via upload

---
+++ pekan2/Input_2611533015.py
@@ -0,0 +1,10 @@
+# Buat file dengan nama Input_261153015.py
+# Buat Program untuk menghitung luas persegi panjang
+# Nama variabel ditambah 4 digit mnimm terakhir contoh: nama_3015
+# Program ini menggunakan fungsi input() untuk meminta pengguna
+# memasukan nilai panjang dan lebar persegi panjang
+# Nilai yang dimasukkan akan dikonversi menjadi tipe data integer
+panjang_3015 = input("Masukkan nilai panjang: ")
+lebar_3015 = input("Masukkan nilai lebar: ")
+luas_3015 = int(panjang_3015) * int(lebar_3015)
+print("Luas = ",luas_3015)
---
+++ pekan2/String_2611533015.py
@@ -0,0 +1,16 @@
+# Buat file dengan nama string String_2611533015.py
+# nama variabel ditambah 4 digit terakhir contoh: nama_3015
+# ganti programnya dan data masing2
+nama_3015 = "Zikri"
+jenis_kelamin_3015 = 'Laki-Laki'
+alamat_3015 = """
+    ASR TNI-AD GANTING,
+    Ganting, Padang,
+    Sumatera Barat, Indonesia
+"""
+
+agama_3015 = "Islam"
+print("Nama saya adalah: ", nama_3015)
+print("Jenis kelamin saya adalah: ", jenis_kelamin_3015)
+print("Alamat saya adalah: ", alamat_3015)
+print("Agama saya adalah: ", agama_3015)
